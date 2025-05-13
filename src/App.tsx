197e8881b4f06d301c3c6cbc35f4d427b7661339
Hervé Dombya <135591453+hervedombya@users.noreply.github.com>
2025-05-13 10:15:30 +0200

    Initialize React project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>Dummy UI</h1>
+      <p>This is a dummy UI</p>
+    </div>
+  );
+};
+
+export default App;
